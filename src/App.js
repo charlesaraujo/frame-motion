4579bbf22adec480260c57cf0e2952281d409f8c
Charles Araújo <charles_araujo@ymail.com>
2021-10-18 03:30:09 -0300

    new: add launch button and backdrop component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { motion } from "framer-motion";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.9 }}
+        className="save-button"
+        onClick={() => null}
+      >
+        Abrir modal
+      </motion.button>
+    </div>
+  );
 }
 export default App;
