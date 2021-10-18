a215b0c3f509370b7b3a894087508330b0aa56dc
Charles Araújo <charles_araujo@ymail.com>
2021-10-18 03:51:12 -0300

    release: finish mini study about frame motion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { motion } from "framer-motion";
-
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import Modal from "./components/Modal";
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
   return (
     <div>
       <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.9 }}
         className="save-button"
-        onClick={() => null}
+        onClick={() => (modalOpen ? close() : open())}
       >
         Abrir modal
       </motion.button>
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+      >
+        {modalOpen && (
+          <Modal modalOpen={modalOpen} handleClose={close} text="Opaaa" />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
--- src/components/Backdrop/index.jsx
+++ src/components/Backdrop/index.jsx
@@ -2,7 +2,13 @@ import { motion } from "framer-motion";
 
 const Backdrop = ({ children, onClick }) => {
   return (
-    <motion.div className="backdrop" onClick={onClick}>
+    <motion.div
+      className="backdrop"
+      onClick={onClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       {children}
     </motion.div>
   );
